490fe3fc0d4b7870d1ee9bd0e8edb339ae733464
unknown <dbehera647@gmail.com>
2024-12-07 12:26:32 +0530

    Authenticating the user

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.post("/signup", async (req, res) => {
     validateSignupData(req);
 
     const { firstName, lastName, emailId, password } = req.body;
-    //encrypt password
+    //encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
     console.log(passwordHash);
 
@@ -33,6 +33,25 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//login the user
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials!");
+    }
+    const isPasswordValid = await bcrypt.compare(password,user.password)
+
+    if(isPasswordValid){
+      res.send("Login Successful !!!")
+    }else{
+      throw new Error("Invalid credentials")
+    }
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
 //Getting all users from db
 app.get("/users", async (req, res) => {
   try {
