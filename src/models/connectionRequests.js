86692e85fabdf54d01153d735dc1017c291275e8
unknown <dbehera647@gmail.com>
2024-12-13 20:27:29 +0530

    Using pre method of schema to check if the user is sending request to himself

--- src/models/connectionRequests.js
+++ src/models/connectionRequests.js
@@ -23,5 +23,14 @@ const connectionRequestSchema = new mongoose.Schema({
     timestamps : true
 })
 
+
+  connectionRequestSchema.pre("save",function(next){
+    const connectionRequest = this;
+    //checking if teh fromUserId is equals to toUserId
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to yourself!!")
+    }
+    next()
+  })
 const ConnectionRequestModel = new mongoose.model("ConnectionRequest",connectionRequestSchema);
 module.exports = {ConnectionRequestModel};
