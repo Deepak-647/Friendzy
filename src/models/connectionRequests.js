365943ed73d7472e338259b00889b840d25deb70
unknown <dbehera647@gmail.com>
2024-12-21 13:41:03 +0530

    Created userRouter to get the requests and used ref & populate to show their name

--- src/models/connectionRequests.js
+++ src/models/connectionRequests.js
@@ -1,39 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema({
-    fromUserId:{
-        type : mongoose.Schema.Types.ObjectId,
-        required :true
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref : "User",
+      required: true,
     },
-    toUserId : {
-        type:mongoose.Schema.Types.ObjectId,
-        required: true
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    status:{
-        type:String,
-        required:true,
-        enum: {
-            values:["ignored","interested","accepted","rejected"],
-            message :`{VALUE} is incorrect status type`
-        }
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-    }
-},
-{
-    timestamps : true
-})
+//Creating compund indexes for FromUserId and toUserId for better db query
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
- //Creating compund indexes for FromUserId and toUserId for better db query
- connectionRequestSchema.index({fromUserId:1,toUserId:1});
-
- 
-   connectionRequestSchema.pre("save",function(next){
-    const connectionRequest = this;
-    //checking if teh fromUserId is equals to toUserId
-    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
-        throw new Error("Cannot send connection request to yourself!!")
-    }
-    next()
-  })
-const ConnectionRequestModel = new mongoose.model("ConnectionRequest",connectionRequestSchema);
-module.exports = {ConnectionRequestModel};
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  //checking if teh fromUserId is equals to toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!!");
+  }
+  next();
+});
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+module.exports = { ConnectionRequestModel };
