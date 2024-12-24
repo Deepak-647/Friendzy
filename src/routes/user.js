d63fb2f387f27e2ca062bf4c5e08e917e9bad4bf
unknown <dbehera647@gmail.com>
2024-12-24 18:03:04 +0530

    Created the /feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { userAuth } = require("../middlewares/authMiddleware");
 const { ConnectionRequestModel } = require("../models/connectionRequests");
 const userRouter = express.Router();
+const User = require("../models/user");
 const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl"];
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -34,10 +36,10 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("toUserId", USER_SAFE_DATA);
 
     const connections = connectionRequest.map((row) => {
-        if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
-            return row.toUserId;
-        }
-        return row.fromUserId
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
     });
     res.send({ connections });
   } catch (err) {
@@ -45,4 +47,34 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    //Getting the loggedInUser
+    const loggedInUser = req.user;
+
+    //Finding all the previous connection requests
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    //Hiding all the connection requests by forming a new set to avoid them to show them in the field
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    //Getiing all the users except those which has a previous conentcion request
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
 module.exports = userRouter;
