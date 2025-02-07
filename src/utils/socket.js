79b156a09f366d322d2d9d93d5221912611b22a0
unknown <dbehera647@gmail.com>
2025-02-07 17:11:57 +0530

    Hashing the roomID

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,36 +1,36 @@
-const socket = require("socket.io")
+const socket = require("socket.io");
+const crypto = require("crypto");
 
+const getSecretRoomId = (userId, toTargetId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, toTargetId].sort().join("_"))
+    .digest("hex");
+};
+const initializeSocket = (server) => {
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
+    },
+  });
 
+  io.on("connection", (socket) => {
+    //handle events
 
-const initializeSocket = (server) =>{
-    const io= socket(server,{
-        cors :{
-          origin : "http://localhost:5173"
-        }
-      });
-      
-      io.on("connection",(socket)=>{
-        //handle events
+    socket.on("joinChat", ({ userId, toTargetId }) => {
+      const roomId = getSecretRoomId(userId, toTargetId);
 
-        socket.on("joinChat",({firstName,userId,toTargetId})=>{
-            const roomId = [ userId,toTargetId].sort().join("_");
-            console.log(firstName , "Joined room : " + roomId)
-            socket.join(roomId)
-        })
+      socket.join(roomId);
+    });
 
-        socket.on("sendMessage",({
-            firstName,
-            userId,
-            toTargetId,
-            text,
-          })=>{
-            const roomId = [userId,toTargetId].sort().join("_");
-            console.log(firstName+ " "+ text)
-            io.to(roomId).emit("newMessageRecieved",{firstName,text})
-          })
+    socket.on("sendMessage", ({ firstName, userId, toTargetId, text }) => {
+      const roomId = getSecretRoomId(userId, toTargetId);
 
-        socket.on("disconnect",()=>{})
-      })
-}
+      io.to(roomId).emit("newMessageRecieved", { firstName, text });
+    });
+
+    socket.on("disconnect", () => {});
+  });
+};
 
 module.exports = initializeSocket;
